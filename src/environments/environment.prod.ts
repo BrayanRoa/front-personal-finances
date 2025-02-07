3d10360c04848cf3b740cf0929a4800f9e727670
brayanroa <brayanandresrl@ufps.edu.co>
2025-02-07 17:50:16 -0500

    netlify file modified

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,  // Set to true when deploying to production
+    API_URL: 'https://personalfinances-production.up.railway.app/api/v1', // Replace with your API endpoint
+    firebase: {
+        "projectId": "",
+        "appId": "",
+        "storageBucket": "",
+        "apiKey": "",
+        "authDomain": "",
+        "messagingSenderId": ""
+    }
+};
