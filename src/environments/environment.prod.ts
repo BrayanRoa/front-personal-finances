9e278a19492dacce719a2f9f62e53729960fa14c
brayanroa <brayanandresrl@ufps.edu.co>
2025-02-07 18:27:09 -0500

    changes in enviroinment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-    production: true,  // Set to true when deploying to production
-    API_URL: 'https://personalfinances-production.up.railway.app/api/v1', // Replace with your API endpoint
+    production: true,
+    API_URL: process.env['BACKEND_URL'],
     firebase: {
-        "projectId": "",
-        "appId": "",
-        "storageBucket": "",
-        "apiKey": "",
-        "authDomain": "",
-        "messagingSenderId": ""
+        projectId: process.env['FIREBASE_PROJECT_ID'],
+        appId: process.env['FIREBASE_APP_ID'],
+        storageBucket: process.env['FIREBASE_STORAGE_BUCKET'],
+        apiKey: process.env['FIREBASE_API_KEY'],
+        authDomain: process.env['FIREBASE_AUTH_DOMAIN'],
+        messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID']
     }
 };
