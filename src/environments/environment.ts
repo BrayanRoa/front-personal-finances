3e7017d30f7b8e3c11cc2fd5886d1f8377c09929
brayanroa <brayanandresrl@ufps.edu.co>
2025-02-07 18:15:32 -0500

    netlify file modified

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    API_URL: '', // Se puede reemplazar con variables en tiempo de ejecución
+    firebase: {
+        projectId: '',
+        appId: '',
+        storageBucket: '',
+        apiKey: '',
+        authDomain: '',
+        messagingSenderId: ''
+    }
+};
