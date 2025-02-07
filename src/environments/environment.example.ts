05a959844eb0ba8efea4c4d4ef7c668410800252
brayanroa <brayanandresrl@ufps.edu.co>
2025-02-07 16:15:00 -0500

    enviroment example adeed

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    API_URL: 'http://localhost:3001/api/v1', // Replace with your API endpoint
+    firebase: {
+        "projectId": "",
+        "appId": "",
+        "storageBucket": "",
+        "apiKey": "",
+        "authDomain": "",
+        "messagingSenderId": ""
+    }
+};
