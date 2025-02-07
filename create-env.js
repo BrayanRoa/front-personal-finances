e81ad74174df94a45f8cd8b35c527a01139721b7
brayanroa <brayanandresrl@ufps.edu.co>
2025-02-07 18:34:35 -0500

    remove credentials

--- create-env.js
+++ create-env.js
@@ -1,19 +1,18 @@
 const fs = require("fs");
-
-const envProd = `
+const environmentProd = `
 export const environment = {
-    production: true,
-    API_URL: '${process.env.API_URL}',
-    firebase: {
-        projectId: '${process.env.FIREBASE_PROJECT_ID}',
-        appId: '${process.env.FIREBASE_APP_ID}',
-        storageBucket: '${process.env.FIREBASE_STORAGE_BUCKET}',
-        apiKey: '${process.env.FIREBASE_API_KEY}',
-        authDomain: '${process.env.FIREBASE_AUTH_DOMAIN}',
-        messagingSenderId: '${process.env.FIREBASE_MESSAGING_SENDER_ID}'
-    }
+  production: true,
+  API_URL: '${process.env.NG_APP_API_URL}',
+  firebase: {
+    projectId: '${process.env.NG_APP_FIREBASE_PROJECT_ID}',
+    appId: '${process.env.NG_APP_FIREBASE_APP_ID}',
+    storageBucket: '${process.env.NG_APP_FIREBASE_STORAGE_BUCKET}',
+    apiKey: '${process.env.NG_APP_FIREBASE_API_KEY}',
+    authDomain: '${process.env.NG_APP_FIREBASE_AUTH_DOMAIN}',
+    messagingSenderId: '${process.env.NG_APP_FIREBASE_MESSAGING_SENDER_ID}'
+  }
 };
 `;
 
-fs.writeFileSync("./src/environments/environment.prod.ts", envProd);
-console.log("✔️ Archivo environment.prod.ts generado correctamente");
+fs.writeFileSync("./src/environments/environment.prod.ts", environmentProd);
+console.log("✅ Archivo environment.prod.ts generado correctamente.");
